73159948d4d368c7cb9b5fd99986a8c0a9af138e
Daniel Garnier-Moiroux <git@garnier.wf>
2025-04-18 13:15:16 +0200

    Add resource server to MCP server, do not turn on auth

--- mcp-weather-webmvc-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
+++ mcp-weather-webmvc-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
@@ -1,20 +1,41 @@
 package org.springframework.ai.mcp.sample.server;
 
+import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @SpringBootApplication
 public class McpServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(McpServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerApplication.class, args);
+    }
 
-	@Bean
-	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
-		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
-	}
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // Required for now, we can't do request-level security
+                // because the authorization depends on the
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2ResourceServer(res -> res.jwt(Customizer.withDefaults()))
+                .csrf(CsrfConfigurer::disable)
+                .build();
+    }
+
 }

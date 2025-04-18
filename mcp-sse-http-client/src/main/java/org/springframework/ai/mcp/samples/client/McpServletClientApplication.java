799c4c7654c0233d926cede60411dd26479a220c
Daniel Garnier-Moiroux <git@garnier.wf>
2025-04-18 13:14:52 +0200

    Update mcp client to get tokens

--- mcp-sse-http-client/src/main/java/org/springframework/ai/mcp/samples/client/McpServletClientApplication.java
+++ mcp-sse-http-client/src/main/java/org/springframework/ai/mcp/samples/client/McpServletClientApplication.java
@@ -16,18 +16,28 @@
 package org.springframework.ai.mcp.samples.client;
 
 import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.TokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @SpringBootApplication
 public class McpServletClientApplication {
@@ -43,18 +53,38 @@ public class McpServletClientApplication {
                 .build();
     }
 
-//    @Bean
-//    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder, List<McpSyncClient> mcpClients) {
-//
-//        return args -> {
-//
-//
-//            System.out.println("Response: " +
-//                               chatClient.prompt("What is the weather in Amsterdam right now?")
-//                                       .call()
-//                                       .content());
-//        };
-//    }
+    @Bean
+    TokenProvider tokenProvider(OAuth2AuthorizedClientManager authorizedClientManager) {
+        return () -> {
+            Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+            if (principal == null) {
+                principal = new AnonymousAuthenticationToken("anonymous",
+                        "anonymousUser", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+            }
+
+            var requestAttributes = RequestContextHolder.getRequestAttributes();
+            if (!(requestAttributes instanceof ServletRequestAttributes servletRequestAttributes)) {
+                return null;
+            }
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            HttpServletResponse response = servletRequestAttributes.getResponse();
+
+            // @formatter:off
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                    // matches spring.security.oauth2.client.registration.<REGISTRATION-ID>
+                    // from application.properties
+                    .withClientRegistrationId("authserver")
+                    .principal(principal)
+                    .attribute(HttpServletRequest.class.getName(), request)
+                    .attribute(HttpServletResponse.class.getName(), response)
+                    .build();
+            // @formatter:on
+
+            return authorizedClientManager.authorize(authorizeRequest)
+                    .getAccessToken()
+                    .getTokenValue();
+        };
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {

d85930ae45c46a0719693bab01b88fc7319d2476
Daniel Garnier-Moiroux <git@garnier.wf>
2025-06-13 17:10:35 +0200

    Start working on mcp-sse-webflux-http-client

---
+++ mcp-sse-webflux-http-client/src/main/java/org/springframework/ai/mcp/samples/webflux/httpclient/DemoController.java
@@ -0,0 +1,74 @@
+package org.springframework.ai.mcp.samples.webflux.httpclient;
+
+import javax.validation.constraints.NotNull;
+
+import java.util.stream.Collectors;
+import reactor.core.publisher.Mono;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class DemoController {
+
+	private final ChatClient chatClient;
+
+	public DemoController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+
+	private static boolean test(ChatResponse cr) {
+		return cr.getResults().stream().anyMatch(r -> r.getOutput().hasToolCalls());
+	}
+
+	@GetMapping("/")
+	Mono<String> index(String query) {
+		String questionForm = """
+				<h1>Demo controller</h1>
+				%s
+				<h2>Ask for the weather</h2>
+				<p>In which city would you like to see the weather?</p>
+				<form action="" method="GET">
+				    <input type="text" name="query" value="" placeholder="Paris" />
+					<button type="submit">Ask the LLM</button>
+				</form>
+				""";
+		return Mono.justOrEmpty(query)
+			.flatMap(this::getChatResponseDiscardingOutputBeforeToolCall)
+			.switchIfEmpty(Mono.just(""))
+			.map(questionForm::formatted);
+	}
+
+	// The response we get from the .stream() API is too long. It contains the first
+	// output of the LLM ("I need to use this and that tool"), as well as the second
+	// output with the tool result itself.
+	// This is a hack, which discards everything the chat client generated before a tool
+	// was called.
+	private Mono<String> getChatResponseDiscardingOutputBeforeToolCall(@NotNull String query) {
+		return this.chatClient.prompt("What is the weather in %s right now?".formatted(query))
+			.stream()
+			.chatResponse()
+			// Discard everything before the first "tool call"
+			.skipUntil(cr -> cr.getResults().stream().anyMatch(r -> r.getOutput().hasToolCalls()))
+			.flatMapIterable(ChatResponse::getResults)
+			// Only keep textual output
+			.mapNotNull(Generation::getOutput)
+			.mapNotNull(AssistantMessage::getText)
+			.collect(Collectors.joining(" "))
+			// TODO: add tokenPublisher in the context
+			.contextWrite(ctx -> ctx.put("tokenPublisher", null));
+	}
+
+	// Ideally we want to use this
+	private Mono<String> getChatResponse(@NotNull String query) {
+		return this.chatClient.prompt("What is the weather in %s right now?".formatted(query))
+			.stream()
+			.content()
+			.collect(Collectors.joining(" "));
+	}
+
+}
